1f99b78207e186c900727e97a705b01138600ee3
leyan95 <18231926271@163.com>
2020-12-10 14:26:16 +0800

    build: upgrade

--- src/main/java/org/hv/template/TemplateInitialization.java
+++ src/main/java/org/hv/template/TemplateInitialization.java
@@ -1,7 +1,9 @@
 package org.hv.template;
 
+import org.hv.biscuits.core.BiscuitsConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,15 +12,30 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * @author wujianc
- */
-@Configuration
-@EnableSwagger2
-public class TemplateSwagger {
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
+/**
+ * @author wujianchuan 2020/7/9 10:45
+ */
+@Component
+@EnableSwagger2
+public class TemplateInitialization {
     public static final String SWAGGER_SCAN_BASE_PACKAGE = "org.hv.template.adapters";
     public static final String VERSION = "1.0.0";
+
+    @Value("${biscuits.withPersistence:true}")
+    private boolean withPersistence;
+
+    @Resource
+    private BiscuitsConfig biscuitsConfig;
+
+    @PostConstruct
+    public void initBis() throws Exception {
+        if (withPersistence) {
+            biscuitsConfig.setDesKey("sward007").setSm4Key("sward18713839007").init();
+        }
+    }
 
     @Bean
     public Docket docket() {
